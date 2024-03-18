538882ffbc228730f311046e4490291a2ac9e691
s28610 <s28610@pjwstk.edu.pl>
2024-03-18 04:09:16 +0300

    feat: add method counting average

--- APBD_LAB2/APBD_LAB2/Program.cs
+++ APBD_LAB2/APBD_LAB2/Program.cs
@@ -1,6 +1,20 @@
 ﻿class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        int[] numbers = { 2, 4, 6, 8, 10 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Average: " + average);
+    }
+
+    static double CalculateAverage(int[] array)
+    {
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
     }
 }
